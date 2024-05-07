37cd23e38d8b078864d505a9c861387d28c41e89
gsus <satehortuamercado@gmail.com>
2024-05-07 06:42:53 -0500

    database populated

--- src/controller/searchController.ts
+++ src/controller/searchController.ts
@@ -1,5 +1,5 @@
 import { EQM } from "../types"
-import db from "../config/db"
+import db from "../db"
 
 export const suggestionSearch: EQM = async (request, response, next) => {
   const result = await db.query("SELECT NOW()")
